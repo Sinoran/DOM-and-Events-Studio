843bb96c1940dafc0278f1abafd6ffe770e8be1b
Scott Sullentrop <sscalex@yahoo.com>
2020-07-08 19:28:09 -0500

    2nd attempt

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function init () {
     button = document.getElementById("takeoff")
 
     button.addEventListener("click", function() {
-        alert("Confirm that the shuttle is ready for takeoff.")
+        window.alert("Confirm that the shuttle is ready for takeoff.")
       });
 }
 
