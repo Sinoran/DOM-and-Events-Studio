d84796e83a148e587be88ab2e5428c3d11aeb3d6
Scott Sullentrop <sscalex@yahoo.com>
2020-07-08 20:11:02 -0500

    added a few events

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
+window.addEventListener("load", function() {
+
 let button = null;
+let flightstatus = null;
+let background = null;
+let spaceShuttleHeight = null;
 
 function init () {
-    button = document.getElementById("takeoff")
+    button = document.getElementById("takeoff");
+    flightstatus = document.querySelector("p");
+    background = document.getElementById("shuttleBackground");
+    spaceShuttleHeight = document.querySelector("p")
+
+    button.addEventListener("click", function() {
+        flightstatus.innerHTML = "Shuttle in flight"
+    });
 
     button.addEventListener("click", function() {
         window.alert("Confirm that the shuttle is ready for takeoff.")
-      });
-}
+    });
 
+    button.addEventListener("click", function() {
+        background.style.background = "blue"
+    });
 
+    button.addEventListener("Click", function() {
+
+    })
+  }
+})
+window.onload = init;
 
